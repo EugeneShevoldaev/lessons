f7991df195dd6b82ad245e02b0caa3c067993a2f
EugeneShevoldaev <161762648+EugeneShevoldaev@users.noreply.github.com>
2024-12-10 20:30:14 +0300

    Add files via upload

---
+++ pythonProjects/lessons/e-mail_sender.py
@@ -0,0 +1,28 @@
+def send_email(message='Привет!', recipient='pupkin@mail.ru',*,sender="university.help@gmail.com"):
+    # проверка на наличие символа '@'
+    if '@' not in sender or '@' not in recipient:
+        print(f'не возможно отправить письмо с адреса {sender} на адрес {recipient} нет собаки')
+        return
+    # проверка наличия доменов
+    valid_domains = ['.ru', '.com', '.net']
+    resolt_for_sender = any(domain in sender for domain in valid_domains)
+    resolt_for_recipient = any(domain in recipient for domain in valid_domains)
+    resolt = resolt_for_sender + resolt_for_recipient
+    if resolt < 2:
+        print(f'не возможно отправить письмо с адреса {sender} на адрес {recipient} нет домена')
+        return
+
+
+    # проверка на отправку себе
+    if sender == recipient:
+        print('не возможно отправить письмо самому себе')
+        resolt = resolt - 1
+
+    if sender != "university.help@gmail.com":
+        print(f'не стандартный отправитель, письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+        resolt = resolt + 1
+    if resolt == 2:
+        print(f'письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email()
