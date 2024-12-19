ca1290860843de134faf322fdc5b5c34b7a9d59f
EugeneShevoldaev <161762648+EugeneShevoldaev@users.noreply.github.com>
2024-12-19 22:45:47 +0300

    Add files via upload

--- pythonProjects/lessons/module_4_1.py
+++ pythonProjects/lessons/module_4_1.py
@@ -1,12 +1,8 @@
-from  fake_math import divide as fake_div
-from true_math import  divide as true_div
+from fake_math import divide as fake_div
+from true_math import divide as true_div
 
-
-
-
-resolt_fake = fake_div(3,0)
+resolt_fake = fake_div(3, 0)
 print(f'fake_div: -  {resolt_fake}')
 
-
-resolt_true =  true_div(15,0)
+resolt_true = true_div(15, 0)
 print(f'true_div: -  {resolt_true}')
--- pythonProjects/lessons/true_math.py
+++ pythonProjects/lessons/true_math.py
@@ -1,14 +1,15 @@
-from  math import  inf
-# first =int(input('введите  делимое: '))
-# second =int(input('введите  делитель: '))
+from math import inf
 
 first = 49
 second = 7
+
+
 def divide(first, second):
     if second == 0:
-       return inf
+        return inf
     else:
         resolt = round(first / second, 2)
-    return  resolt
+    return resolt
+
 
 print(divide(first, second))
